132e29d640971c31932e5e77ac2cc209df361560
kenxian20 <kenxian20@gmail.com>
2024-03-25 00:51:04 +0800

    新增首頁page

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
+import Header from './homePage/Header';
+import Slide from './homePage/Slide';
+import Slide2 from './homePage/Slide2';
 
-function Home(){
-    return <h1>home page</h1>
+
+class Home extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div className="container">
+                <Header/>
+                <br />
+                <br />
+                <br />
+                <Slide />
+                <Slide2 />
+                <br />
+                <br />
+                <br />
+            </div>
+        );
+    }
 }
+ 
 export default Home;
