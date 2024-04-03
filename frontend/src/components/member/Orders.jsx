945abf711ded9c819bd9f12daefb288dbe730823
lily12326 <64433688+lily12326@users.noreply.github.com>
2024-04-03 15:58:26 +0800

    final update

--- frontend/src/components/member/Orders.jsx
+++ frontend/src/components/member/Orders.jsx
@@ -5,17 +5,6 @@ import "@fontsource/inter";
 import OrdersList from "./OrdersListAll.jsx";
 import { FormControl, MenuItem, TextField } from "@mui/material";
 
-/* {
-  "orderId": 5,
-  "Uid": 10,
-  "name": "台中赫絲珀HSR高鐵行旅 Hesper Hotel",
-  "startDate": "2024-05-10T16:00:00.000Z",
-  "endDate": "2024-05-27T16:00:00.000Z",
-  "price": 1020,
-  "hotelId": 2,
-  "photo_urls": "https://cf.bstatic.com/xdata/images/hotel/max1024x768/250509585.jpg?k=045b1c581ca6faa9c656dae02f404decd7fec21845fcdbe7e8cd21f3e8d74221&o=&hp=1, https://cf.bstatic.com/xdata/images/hotel/max1024x768/179241673.jpg?k=bbd696940a0974e0c5bb9a65011a400b018423ee333c1f6081a44130dd73de34&o=&hp=1, https://cf.bstatic.com/xdata/images/hotel/max1024x768/182105364.jpg?k=9b0ba142d9a5a8fd14dcc1fb3bf0704c99c0cabfc1a4b1356f01bd1903a039c3&o=&hp=1, https://cf.bstatic.com/xdata/images/hotel/max1024x768/256677827.jpg?k=3eb3c25bee1f776eda991c404d309d5b739c1fe59ca86a07a00406c40b420ac3&o=&hp=1, https://cf.bstatic.com/xdata/images/hotel/max1024x768/182105495.jpg?k=0d69f4a7973e1d9db55066b57077a7e887f2a4b1ab9652a78d7ee91ce26f6d47&o=&hp=1, https://cf.bstatic.com/xdata/images/hotel/max1024x768/182105411.jpg?k=303a5d976bbd7821f659613d4bce0fbb5d0da00c09c5479dd61ebb33677ad54d&o=&hp=1"
-}, */
-
 function Orders({ userId }) {
   const [orders, setOrders] = useState([]);
   const [filterOption, setFilterOption] = useState("all");
